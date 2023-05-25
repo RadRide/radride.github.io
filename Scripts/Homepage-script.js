7f79314cb05f9923bde5bd5f063cfb39006c23c9
RadRide <assaf_gmrad@hotmail.com>
2023-05-25 22:13:05 +0300

    deleting everything

--- Scripts/Homepage-script.js
+++
@@ -1,9 +0,0 @@
-let username;
-document.getElementById("start").onclick = function (){
-    username = document.getElementById("username").value;
-    if(username == ""){
-        window.alert("You need to enter a user name to proceed")
-    }else{
-        window.location.href = "Quiz.html?username=" + encodeURIComponent(username)
-    }
-}
