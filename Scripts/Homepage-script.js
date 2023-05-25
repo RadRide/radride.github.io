3d5bb3a5f53284f87283f1dcc669e5b2c3b18982
RadRide <assaf_gmrad@hotmail.com>
2023-05-25 22:33:05 +0300

    Update Homepage-script.js

--- Scripts/Homepage-script.js
+++ Scripts/Homepage-script.js
@@ -1,9 +1,19 @@
-let username;
-document.getElementById("start").onclick = function (){
-    username = document.getElementById("username").value;
-    if(username == ""){
+const username = document.getElementById("username");
+let enteredName;
+document.getElementById("start").onclick = checkUsername;
+
+username.addEventListener("keydown", event=>{
+    if(event.key == "Enter"){
+        event.preventDefault();
+        checkUsername();
+    }
+})
+
+function checkUsername(){
+    enteredName = username.value;
+    if(enteredName == ""){
         window.alert("You need to enter a user name to proceed")
     }else{
-        window.location.href = "Quiz.html?username=" + encodeURIComponent(username)
+        window.location.href = "Quiz.html?username=" + encodeURIComponent(enteredName)
     }
 }
